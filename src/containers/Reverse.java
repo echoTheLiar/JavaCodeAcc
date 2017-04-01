be1523eca1262345552534151f31355de161dfb6
Liu Yuning <liu_yuning@foxmail.com>
2017-04-01 19:44:34 +0800

    edit author info

--- src/containers/Reverse.java
+++ src/containers/Reverse.java
@@ -8,54 +8,55 @@ import java.util.Iterator;
 /**
  * 利用迭代器实现反转操作
  * 
- * @author liuyuning
+ * @author liu yuning
  * 
  *
  */
 public class Reverse {
-	@SuppressWarnings("serial")
-	private static class ReverseCollection<T> extends ArrayList<T> {
+    @SuppressWarnings("serial")
+    private static class ReverseCollection<T> extends ArrayList<T> {
 
-		public ReverseCollection(Collection<T> collection) {
-			super(collection);
-		}
-
-		public Iterable<T> reverse() {
-			return new Iterable<T>() {
-
-				@Override
-				public Iterator<T> iterator() {
-					return new Iterator<T>() {
-						int current = size() - 1;
-
-						@Override
-						public boolean hasNext() {
-							return current > -1;
-						}
-
-						@Override
-						public T next() {
-							return get(current--);
-						}
-					};
-				}
-			};
-		}
-
+	public ReverseCollection(Collection<T> collection) {
+	    super(collection);
 	}
 
-	public static String reverseWords(String s) {
-		ReverseCollection<String> reverseCollection = new ReverseCollection<String>(Arrays.asList(s.split(" ")));
-		StringBuilder stringBuilder = new StringBuilder();
-		for (String word : reverseCollection.reverse()) {
-			stringBuilder.append(word + " ");
+	public Iterable<T> reverse() {
+	    return new Iterable<T>() {
+
+		@Override
+		public Iterator<T> iterator() {
+		    return new Iterator<T>() {
+			int current = size() - 1;
+
+			@Override
+			public boolean hasNext() {
+			    return current > -1;
+			}
+
+			@Override
+			public T next() {
+			    return get(current--);
+			}
+		    };
 		}
-		return stringBuilder.toString().trim();
-
+	    };
 	}
 
-	public static void main(String[] args) {
-		String s = "This is a test for reversing words";
-		System.out.println(reverseWords(s));
+    }
+
+    public static String reverseWords(String s) {
+	ReverseCollection<String> reverseCollection = new ReverseCollection<String>(
+		Arrays.asList(s.split(" ")));
+	StringBuilder stringBuilder = new StringBuilder();
+	for (String word : reverseCollection.reverse()) {
+	    stringBuilder.append(word + " ");
 	}
+	return stringBuilder.toString().trim();
+
+    }
+
+    public static void main(String[] args) {
+	String s = "This is a test for reversing words";
+	System.out.println(reverseWords(s));
+    }
 }
